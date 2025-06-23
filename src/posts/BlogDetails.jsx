9b0aeb606b39d4dd652a9d7f0f4dda7f443a4f5b
mugisha isaie <mugishaisaieog1@gmail.com>
2025-06-23 15:16:03 +0200

    adding some starter blogs into my portifolio

--- src/posts/BlogDetails.jsx
+++ src/posts/BlogDetails.jsx
@@ -4,7 +4,10 @@ import content from '../posts/what_you_need_to_start_web_development.md?raw'
 export default function BlogDetails() {
   return (
     <div className="px-4 py-10 max-w-4xl mx-auto">
-      <h1 className="text-4xl font-bold mb-6 text-center">📚 Blog Post</h1>
+      <h1 className="text-4xl font-bold mb-6 text-center">My 📚 Blog Post</h1>
+      <h3 className='text-center m-5 font-bold text-2xl'>Let's Learn together and Grow together</h3>
+<p className='p-5 font-medium'>I believe that the super thing that can make you super is just learning</p>
+<p className='m-2 text-md bg-slate-300 p-2'>Whether you're starting from scratch or aiming higher, learning is your most powerful tool. Let's explore it together.</p>
       <div className="prose prose-lg">
         <Markdown>{content}</Markdown>
       </div>
